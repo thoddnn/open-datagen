a54da86022d56eb04744b14dc0fa044e5a0871eb
Thomas <thomas@superchargeloop.com>
2024-02-22 23:30:11 +0100

    image to text capability

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thoddnn/open-datagen",
-    version='0.0.34',
+    version='0.0.35',
     packages=find_packages(),
     include_package_data=True,  
     install_requires=[
